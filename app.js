97b551a6468cce57e7e5ec13d9b10c4342fd2894
Nwafor-Onyebuchi <buchilazarus4@gmail.com>
2020-12-07 05:54:26 +0100

    delete record from firestore

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
 const cafeList = document.getElementById("cafe-list");
+const form = document.getElementById("add-cafe-form");
+
 function renderData(doc) {
   let li = document.createElement("li");
   let name = document.createElement("span");
   let city = document.createElement("span");
-  li.setAttribute("data-id", doc.id);
+  let cross = document.createElement("div");
 
-  li.appendChild(name);
-  li.appendChild(city);
   name.textContent = doc.data().name;
   city.textContent = doc.data().city;
+  cross.textContent = "x";
+
+  li.setAttribute("data-id", doc.id);
+  li.appendChild(name);
+  li.appendChild(city);
+  li.appendChild(cross);
+
   cafeList.appendChild(li);
+
+  cross.addEventListener("click", (e) => {
+    e.stopPropagation();
+    let id = e.target.parentElement.getAttribute("data-id");
+    db.collection("cafes").doc(id).delete();
+  });
 }
 
+// get data from firestore
 db.collection("cafes")
   .get()
   .then((snapshot) => {
@@ -20,3 +34,14 @@ db.collection("cafes")
     });
     // console.log(snapshot.docs);
   });
+
+// save data to firestore
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  db.collection("cafes").add({
+    name: form["name-input"].value,
+    city: form["city-input"].value,
+  });
+  form["name-input"].value = "";
+  form["city-input"].value = "";
+});
